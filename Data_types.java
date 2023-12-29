e51bea70cfb862239aba254a8a0341cd7e922931
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2023-12-29 19:51:27 +0530

    Create Data_types.java
    Data types in java....
    primitive and non primitive(Reference)
    *Primitive:
    integer-byte,boolean,int,short,long,char
    floating point-float,double
    *Reference:
    Arrays,Interfaces,Classes

---
+++ Data_types.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class akila{
+    public static void main(String args[])
+    {
+
+        byte b1=1,b2=5;
+        System.out.println("The addition of byte:"+(b1+b2));
+        boolean bo1=true,bo2=false;
+        System.out.println("The boolean values:"+bo1+" "+bo2);
+        short s1=10000,s2=-5000;
+        System.out.println("The short values:"+(s1+s2));
+        int a=10000,b=-20000;
+        System.out.println("The integer values:"+(a+b));
+        long l1=1000000,l2=-2000000;
+        System.out.println("The Long values:"+(l1+l2));
+        float f1=3.0f,f2=3.6f;
+        System.out.println("The float values:"+(f1+f2));
+        double d1=12.3,d2=31.2;
+        System.out.println("The double values:"+(d1+d2));
+        char c1='A';
+        System.out.println("The character is:"+c1);
+}}
